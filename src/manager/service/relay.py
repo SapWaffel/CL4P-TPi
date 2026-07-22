a2d9257edd098e0ba6fecd06847aacab2172afa9
SapWaffel <sebastian@ag-templer.de>
2026-07-22 19:28:54 +0200

    Feat: add logging to send_boot_signal for better traceability

--- src/manager/service/relay.py
+++ src/manager/service/relay.py
@@ -1,8 +1,11 @@
 import RPi.GPIO as GPIO
 import time
+import logging
 
+logger = logging.getLogger(__name__)
 
 def send_boot_signal():
+    logger.info("Relay triggered.")
     # Set GPIO-Modus (BCM)
     GPIO.setmode(GPIO.BCM)
     RELAY_PINS = [7, 3, 22, 25]
@@ -11,11 +14,13 @@ def send_boot_signal():
         GPIO.setup(pin, GPIO.OUT)
         GPIO.output(pin, GPIO.LOW)
 
+    logger.info("Activating relay.")
     # Relais einschalten
     GPIO.output(RELAY_PINS[2], GPIO.HIGH)
     time.sleep(0.1)
     GPIO.output(RELAY_PINS[2], GPIO.LOW)
 
+    logger.info("Cleaning up GPIO.")    
     # Aufräumen
     GPIO.cleanup()
     return {"success": True}
